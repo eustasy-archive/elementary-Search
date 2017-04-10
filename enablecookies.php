7d7290e9bd80b0d367c4539219368c5dfee25197
Lewis Goddard <lewisgoddard@eustasy.org>
2017-04-10 17:51:56 +0100

    Create enablecookies.php

--- enablecookies.php
+++ enablecookies.php
@@ -2,4 +2,4 @@
 if ( !isset($_COOKIE['allow_cookies']) || $_COOKIE['allow_cookies'] != 1 ) {
 	setcookie('allow_cookies', '1', time() + 2419200, '/', '.eustasy.org');
 }
-header('Location: http://labs.eustasy.org/elementary-Search/', true, 302);
+header('Location: https://labs.eustasy.org/elementary-Search/', true, 302);
