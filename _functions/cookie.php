3caadd52ca7705790f278d7b9aa186e153735e9d
Lewis Goddard <lewisgoddard@eustasy.org>
2018-05-17 16:46:41 +0100

    cookie_is accesses the super-global variable $_COOKIE.

--- _functions/cookie.php
+++ _functions/cookie.php
@@ -1,9 +1,10 @@
 <?php
 
 function cookie_is(string $cookie_name, string $cookie_value, string $return_if_true = ' checked'): string {
+	$cookie = filter_input(INPUT_COOKIE, $cookie_name);
 	if (
-		isset($_COOKIE[$cookie_name]) &&
-		$_COOKIE[$cookie_name] == $cookie_value
+		isset($cookie) &&
+		$cookie == $cookie_value
 	) {
 		return $return_if_true;
 	}
